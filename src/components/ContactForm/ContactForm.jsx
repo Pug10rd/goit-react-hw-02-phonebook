bbdc4591095c58392d029063e066f1eda618a69d
Ivan Zarichnyi <91795849+Pug10rd@users.noreply.github.com>
2023-03-28 18:22:03 +0200

    commit 2

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -29,13 +29,3 @@ export function ContactForm({addContact}) {
     </Formik>
   );
 }
-
-{
-  /* <input
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
-/>; */
-}
